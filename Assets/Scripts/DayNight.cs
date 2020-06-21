1a86c3a470c8f393ecf3ee60989994a49f867d98
PitsCrafts <marit.de.lange@hotmail.nl>
2020-06-21 15:09:46 +0200

    Done

---
+++ Assets/Scripts/DayNight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNight : MonoBehaviour
+{
+    public GameObject ForrestDay;
+    public GameObject LightDay;
+    public bool Day;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed left click.");
+            if (Day == true)
+            {
+                ForrestDay.SetActive(false);
+                LightDay.SetActive(false);
+                Day = false;
+            }
+            else
+            {
+                ForrestDay.SetActive(true);
+                LightDay.SetActive(true);
+                Day = true;
+            }
+        }
+    }
+}
